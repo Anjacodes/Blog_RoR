92cc595a0dfec10bb76674a5b46755d61517466c
Anjacodes <anja_schmidt7@posteo.de>
2022-03-22 12:00:22 -0600

    Add View and Controller for Posts

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,9 @@ Rails.application.routes.draw do
 
   get "/users", to: "users#index"
   get "/users/:id", to: "users#show"
+  get "/users/:id/posts", to: "posts#index"
+  get "/users/:id/posts/3", to: "posts#show"
+  # resources :users do
+  #   resources :posts
+  # end
 end
