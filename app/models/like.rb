4a74ea366531446641a1524bb33af038a4b4618d
Anjacodes <anja_schmidt7@posteo.de>
2022-03-28 18:47:54 -0600

    Linters!

--- app/models/like.rb
+++ app/models/like.rb
@@ -3,7 +3,7 @@ class Like < ApplicationRecord
   belongs_to :user
 
   def post_like_counter
-    post = Post.find(self.post_id)
+    post = Post.find(post_id)
     post.increment!(:likesCounter)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,7 @@ class Post < ApplicationRecord
   has_many :likes
 
   def update_counter
-    user = User.find(self.user_id)
+    user = User.find(user_id)
     user.increment!(:postsCounter)
   end
 
