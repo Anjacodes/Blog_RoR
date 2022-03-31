964e4ed16236ea9d255386f15099ca1c6be57957
Anjacodes <anja_schmidt7@posteo.de>
2022-03-31 17:24:58 -0600

    Remove commented code

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,6 @@
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    # @posts = Post.includes(:author).where(users: { id: params[:user_id] })
     @posts = @user.posts.includes(:comments)
   end
 
